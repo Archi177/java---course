6e3387d0071d5244e2b82e33ba4f551d42afb6ed
Archi177 <afirov@list.ru>
2015-12-08 12:48:30 +0300

    create calculate

--- calculate.java
+++ calculate.java
@@ -1,5 +1,9 @@
 public class calculate{
 	public static void main(String[] args){
 		System.out.println("calculate");
+		int first = Integer.valueOf(args[0]);
+		int second = Integer.valueOf(args[1]);
+		int summ = first + second;
+		System.out.println("sum  " + summ);
 	}
 }
